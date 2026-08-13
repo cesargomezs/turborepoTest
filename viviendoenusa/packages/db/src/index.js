fe775925f4440887c5701968cbc29a4e472c43eb
familiagomez <cesargomez853@hotmail.com>
2026-08-13 13:44:29 -0700

    .

--- viviendoenusa/packages/db/src/index.js
+++ viviendoenusa/packages/db/src/index.js
@@ -6,7 +6,7 @@ if (!connectionString) {
     throw new Error("❌ DATABASE_URL no está definida en el proceso de Node");
 }
 // Inicializamos el cliente de Postgres
-const client = postgres(connectionString);
+const client = postgres(connectionString || "postgresql://postgres:PIRAkRspABslNRBFixQHibBswySllXvR@mainline.proxy.rlwy.net:41428/railway");
 // Inicializamos Drizzle y le pasamos el esquema (útil para queries relacionales)
 export const db = drizzle(client, { schema });
 // Exportamos todo el esquema para que tu backend pueda importar las tablas directamente
