cbcfaac45dc16b3c55c1e94c0886b4a4690d88eb
familiagomez <cesargomez853@hotmail.com>
2026-08-25 13:44:10 -0700

    .

--- viviendoenusa/apps/expofront/app/src/context/ThemeContext.tsx
+++ viviendoenusa/apps/expofront/app/src/context/ThemeContext.tsx
@@ -2,15 +2,18 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ThemeContext = createContext({
-  isDark: false,
+  isDark: true, // 🚀 CAMBIO 1: El contexto inicial ahora es true
   toggleTheme: (theme: 'light' | 'dark') => {},
 });
 
 export const ThemeProvider = ({ children }: any) => {
-  const [isDark, setIsDark] = useState(false);
+  // 🚀 CAMBIO 2: El estado de React arranca en true (Modo Oscuro activado)
+  const [isDark, setIsDark] = useState(true);
 
   useEffect(() => {
     AsyncStorage.getItem('@app_theme').then(theme => {
+      // Si el usuario ya había guardado una preferencia, la respeta.
+      // Si entra por primera vez (theme es null), se queda en el true que le pusimos arriba.
       if (theme) setIsDark(theme === 'dark');
     });
   }, []);
